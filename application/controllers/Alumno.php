5f9b7fce910cd09ba6aab5d954b51cf09c72f57c
Ricardo Cruz Leyva <rcl6395@gmail.com>
2017-05-22 18:06:45 -0500

    FIX VISTAS DOCENTE Y ALUMNO DE AMAURY

--- application/controllers/Alumno.php
+++ application/controllers/Alumno.php
@@ -4,6 +4,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Alumno extends CI_Controller {
 
 	public function index(){
-		$this->load->view('alumno/index');
+		$data = array('page_title'=>'Panel de administración');
+        // $data array que recibe la vista
+        // Contiene los css de la plantilla
+        $this->load->view('layout/head',$data);
+        // Es la barra superior de la plantilla
+        $this->load->view('layout/header');
+        // Este es el contenedor de la plantilla
+        // Este archivo es en el que trabajarán
+        $this->load->view('alumno/index');
+        // Menu lateral
+        $this->load->view('layout/menu');
+        // Js para que funcione la plantilla correctamente
+        $this->load->view('layout/scripts');
 	}
 }
--- application/views/layout/head.php
+++ application/views/layout/head.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="es" moznomarginboxes>
 	<head>
-		<title><?=$page_title ?></title>
+		<title><?=$page_title?></title>
 		<!-- BEGIN META -->
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
