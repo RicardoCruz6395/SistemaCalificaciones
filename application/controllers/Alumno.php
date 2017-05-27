54f92be11088f103f7bfa04941b258a307b6f908
Ricardo Cruz Leyva <rcl6395@gmail.com>
2017-05-26 19:48:52 -0500

    RICARDO CRUZ
    LISTA DE CALIFICACIONES DEL ALUMNO

--- application/controllers/Alumno.php
+++ application/controllers/Alumno.php
@@ -7,38 +7,29 @@ class Alumno extends CI_Controller {
     }
 
 	public function index(){
+    
+        $id_usuario = $this->session->userdata['id'];
+        $this->load->model('usuarios_model');       
+        $alumno = $this->usuarios_model->getAlumno( $id_usuario );
 
-
-		$semestre = '8° Semestre';
-
-		$this->load->model('alumnos_model');
-
-		$data = array('page_title'=>'SC :: Calificaciones ' . $semestre );
-        // $data array que recibe la vista
-        // Contiene los css de la plantilla
-        $this->load->view('layout/head',$data);
-        // Es la barra superior de la plantilla
-        $this->load->view('layout/header');
-        // Este es el contenedor de la plantilla
-        // Este archivo es en el que trabajarán
-        $this->load->view('alumno/index', $materias);
-        // Menu lateral
-        $this->load->view('layout/menu');
-        // Js para que funcione la plantilla correctamente
-        $this->load->view('layout/scripts');
+        $id_alumno = $alumno->ALUM_ALUMNO;
+        $this->load->model('alumnos_model');
+		$periodo = $this->alumnos_model->getUltimoPeriodo( $id_alumno );
+        $periodo = $periodo->GRUP_PERIODO;
+        redirect('alumno/periodo/' . $periodo);
 	}
 
     public function periodo( $periodo = null ){
 
         if( $periodo ){
 
-            $this->load->model('semestres_model');
-            $semestre = $this->semestres_model->getById( 8 );
-
             $this->load->model('alumnos_model');
             $materias = $this->alumnos_model->getMateriasByPeriodo( $periodo );
 
-            $data = array('page_title'=>'SC :: Calificaciones ' . $semestre->SEME_NOMBRE );
+            $data = [
+                'page_title'    =>  'SC :: Panel de Calificaciones',
+                'materias'      =>  $materias
+            ];
             $this->load->view('layout/head',$data);
             $this->load->view('layout/header');
             $this->load->view('alumno/index');
--- application/models/Usuarios_model.php
+++ application/models/Usuarios_model.php
@@ -1,73 +1,66 @@
 <?php
 
-/**
- * Date: 09/05/2017
- * Time: 06:47 PM
- */
-class Usuarios_model extends CI_Model
-{
+class Usuarios_model extends CI_Model{
 
-  public function get()
-  {
-    try {
-      $result = $this->db->get('usuarios');
 
-      if ($result->num_rows() > 0) {
-        return ["success" => true, "response" => $result->result()];
-      } else {
-        return ["success" => false, "response" => "No se pudo obtener datos"];
-      }
-    } catch (Exception $e) {
-      return ["success" => false, "response" => $e->getMessage()];
+    public function getAlumno( $id_usuario ){
+        $this->db->where('ALUM_USUARIO', $id_usuario);
+        return $this->db->get('alumnos')->row();
     }
-  }
 
-  public function get_by_matricula($matricula){
-    try {
-      $this->db->where('USUA_MATRICULA', $matricula);
-      $result = $this->db->get('usuarios');
+    public function get(){
+        try {
+            $result = $this->db->get('usuarios');
 
-      if ($result->num_rows() > 0) {
-        return ["success" => true, "response" => $result->row()];
-      } else {
-        return ["success" => false, "response" => "No se pudo obtener datos"];
-      }
-    } catch (Exception $e) {
-      return ["success" => false, "response" => $e->getMessage()];
+            if ($result->num_rows() > 0)
+                return ["success" => true, "response" => $result->result()];
+            return ["success" => false, "response" => "No se pudo obtener datos"];
+            
+        } catch (Exception $e) {
+            return ["success" => false, "response" => $e->getMessage()];
+        }
     }
-  }
 
+    public function get_by_matricula($matricula){
+        try {
+            $this->db->where('USUA_MATRICULA', $matricula);
+            $result = $this->db->get('usuarios');
 
-  public function get_by_id($id)
-  {
-    try {
-      $this->db->where('id', $id);
-      $result = $this->db->get('usuarios');
+            if ($result->num_rows() > 0)
+                return ["success" => true, "response" => $result->row()];
+            return ["success" => false, "response" => "No se pudo obtener datos"];
 
-      if ($result->num_rows() > 0) {
-        return ["success" => true, "response" => $result->row()];
-      } else {
-        return ["success" => false, "response" => "No se pudo obtener datos"];
-      }
-    } catch (Exception $e) {
-      return ["success" => false, "response" => $e->getMessage()];
+        }catch (Exception $e) {
+            return ["success" => false, "response" => $e->getMessage()];
+        }
     }
-  }
 
-  public function delete($id)
-  {
-    try {
 
-      $this->db->where('id', $id);
-      $this->db->delete("usuarios");
+    public function get_by_id($id){
+        try {
+            $this->db->where('id', $id);
+            $result = $this->db->get('usuarios');
 
-      if ($this->db->affected_rows() > 0) {
-        return ["success" => true, "response" => "Registro eliminado"];
-      } else {
-        return ["success" => false, "response" => "No se pudo obtener datos"];
-      }
-    } catch (Exception $e) {
-      return ["success" => false, "response" => $e->getMessage()];
+            if ($result->num_rows() > 0)
+                return ["success" => true, "response" => $result->row()];
+            return ["success" => false, "response" => "No se pudo obtener datos"];
+
+        } catch (Exception $e) {
+            return ["success" => false, "response" => $e->getMessage()];
+        }
     }
-  }
+
+    public function delete($id){
+        try {
+            $this->db->where('id', $id);
+            $this->db->delete("usuarios");
+
+            if ($this->db->affected_rows() > 0)
+                return ["success" => true, "response" => "Registro eliminado"];
+            return ["success" => false, "response" => "No se pudo obtener datos"];
+          
+        } catch (Exception $e) {
+            return ["success" => false, "response" => $e->getMessage()];
+        }
+    }
 }
