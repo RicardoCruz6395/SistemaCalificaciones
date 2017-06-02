564d51610e161105b23a6401e190f34c978cc380
Ricardo Cruz Leyva <rcl6395@gmail.com>
2017-06-02 00:35:01 -0500

    MAS AVANCES

--- application/views/admin/materias_form.php
+++ application/views/admin/materias_form.php
@@ -1,77 +1,39 @@
-<div class="card card-bordered style-primary">
-    <div class="card-head">
+<div class="card style-primary">
+    <div class="card-head card-head-xs">
         <header><i class="fa fa-book"></i> NUEVA MATERIA</header>
+        <div class="tools">
+			<a class="btn btn-icon-toggle btn-close"><i class="md md-close"></i></a>
+		</div>
     </div><!--end .card-head -->
     <div class="card-body style-default-bright">
-        <form class="form form-validate floating-label" novalidate="novalidate">
-			<div class="card">
-				<div class="card-body">
-					<div class="form-group has-error">
-						<input type="text" class="form-control" id="minmax2" name="minmax2" data-rule-minlength="2" maxlength="5" required="" aria-required="true" aria-describedby="minmax2-error" aria-invalid="true"><span id="minmax2-error" class="help-block">This field is required.</span>
-						<label for="minmax2">Min / Max length</label>
-						<p class="help-block">Minimum length 2 / Maximum length 5</p>
-					</div>
-					<div class="form-group has-error">
-						<input type="text" class="form-control" id="rangelength2" name="rangelength2" data-rule-rangelength="[5, 10]" required="" aria-required="true" aria-describedby="rangelength2-error"><span id="rangelength2-error" class="help-block">This field is required.</span>
-						<label for="rangelength2">Range restriction</label>
-						<p class="help-block">Between 5 and 10</p>
-					</div>
-					<div class="form-group has-error">
-						<input type="text" class="form-control" id="minmaxvalue2" name="minmaxvalue2" data-rule-min="18" data-rule-max="65" required="" aria-required="true" aria-describedby="minmaxvalue2-error"><span id="minmaxvalue2-error" class="help-block">This field is required.</span>
-						<label for="minmaxvalue2">Min / Max value</label>
-						<p class="help-block">Minimum value 18 / Maximum value 65</p>
-					</div>
-					<div class="form-group has-error">
-						<input type="text" class="form-control" id="rangevalue2" name="rangevalue2" data-rule-range="[18, 65]" required="" aria-required="true" aria-describedby="rangevalue2-error"><span id="rangevalue2-error" class="help-block">This field is required.</span>
-						<label for="rangevalue2">Range value</label>
-						<p class="help-block">Between 18 and 65</p>
-					</div>
-					<div class="form-group has-error">
-						<input type="text" class="form-control" id="url2" name="url2" data-rule-url="true" required="" aria-required="true" aria-describedby="url2-error"><span id="url2-error" class="help-block">This field is required.</span>
-						<label for="url2">URL</label>
-						<p class="help-block">Starts with http://</p>
-					</div>
-					<div class="form-group has-error">
-						<input type="text" class="form-control" id="date2" name="date2" data-rule-date="true" required="" aria-required="true" aria-describedby="date2-error"><span id="date2-error" class="help-block">This field is required.</span>
-						<label for="date2">Date</label>
-						<p class="help-block">YYYY-MM-DD</p>
-					</div>
-					<div class="form-group has-error">
-						<input type="text" class="form-control" id="number2" name="number2" data-rule-number="true" required="" aria-required="true" aria-describedby="number2-error"><span id="number2-error" class="help-block">This field is required.</span>
-						<label for="number2">Number</label>
-						<p class="help-block">Numbers only</p>
-					</div>
-					<div class="form-group has-error">
-						<input type="text" class="form-control" id="digits2" name="digits2" data-rule-digits="true" required="" aria-required="true" aria-describedby="digits2-error"><span id="digits2-error" class="help-block">This field is required.</span>
-						<label for="digits2">Digits</label>
-						<p class="help-block">Digits only</p>
-					</div>
-					<div class="form-group has-error">
-						<input type="text" class="form-control" id="creditcard2" name="creditcard2" data-rule-creditcard="true" required="" aria-required="true" aria-describedby="creditcard2-error"><span id="creditcard2-error" class="help-block">This field is required.</span>
-						<label for="creditcard2">Creditcard</label>
-						<p class="help-block">try 446-667-651</p>
-					</div>
-				</div><!--end .card-body -->
-				<div class="card-actionbar">
-					<div class="card-actionbar-row">
-						<button type="submit" class="btn btn-flat btn-primary ink-reaction">Validate</button>
-					</div>
+        <form class="form form-validate floating-label" novalidate="novalidate" id="form-materia">
+			<div class="form-group has-error">
+				<input type="text" class="form-control" id="clave" name="clave" data-rule-minlength="3" maxlength="10" required="" aria-required="true" aria-describedby="clave-error" aria-invalid="true"><span id="clave-error" class="help-block">Información requerida</span>
+				<label for="clave">Clave</label>
+				<p class="help-block">Introduzca 3 a 10 caracteres</p>
+			</div>
+			<div class="form-group has-error">
+				<input type="text" class="form-control" id="materia" name="materia" data-rule-minlength="1" required="" aria-required="true" aria-describedby="materia-error" aria-invalid="true"><span id="materia-error" class="help-block">Información requerida</span>
+				<label for="materia">Nombre de la materia</label>
+			</div>
+			<div class="card-actionbar">
+				<div class="card-actionbar-row">
+					<button type="submit" class="btn btn-primary ink-reaction"><i class="fa fa-floppy-o"></i> GUARDAR</button>
 				</div>
-			</div><!--end .card -->
-			<em class="text-caption">Advanced validation</em>
+			</div>
 		</form>
-    </div><!--end .card-body -->
+    </div>
 </div>
-
-
-
-
-
-
-
-            
 <script type="text/javascript">
-        
-
-
+	$('#form-materia').submit(function(e){
+		var $this = $(this);
+		e.preventDefault();
+		start();
+		
+		if (!$this.valid()) {   // checks form for validity
+        	e.preventDefault();
+        	end();
+        	return false;
+        }
+    });
 </script>
